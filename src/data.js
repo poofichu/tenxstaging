5bfbfd1ad18c5e5809d310e59b4963918b32572a
Jessica Tremblay <poofichu@gmail.com>
2023-04-21 00:31:37 -0500

    footer fallback

--- src/data.js
+++ src/data.js
@@ -12,24 +12,24 @@ export const headerData = {
     },
     {
       text: 'Ten X Playbook',
-    href: getPermalink('/ten-x-playbook'),
+      href: getPermalink('/ten-x-playbook'),
     },
   ],
 };
 
-const navData = [
-  {
-    name: 'Fund Operations',
-    href: getPermalink('/fund-operations'),
-  },
-  {
-    name: 'Founders Stories',
-    href: getBlogPermalink(),
-  },
-  {
-    name: 'Ten X Playbook',
-    href: getPermalink('/ten-x-playbook'),
-  },
-];
-
-export default navData;
+export const footerData = {
+  links: [
+    {
+      text: 'Fund Operations',
+      href: getPermalink('/fund-operations'),
+    },
+    {
+      text: 'Founders Stories',
+      href: getBlogPermalink(),
+    },
+    {
+      text: 'Ten X Playbook',
+      href: getPermalink('/ten-x-playbook'),
+    },
+  ],
+};
