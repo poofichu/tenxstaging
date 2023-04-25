33a16f99344c2ae64be1cc658a884de029e5363e
Jessica Tremblay <poofichu@gmail.com>
2023-04-24 23:59:26 -0500

    script experiment

--- src/scripts/headerActive.js
+++ src/scripts/headerActive.js
@@ -1,5 +1,5 @@
 const nav = document.querySelector(".navbar");
-const navLinks = nav.querySelectorAll("a");
+const navLinks = nav.querySelectorAll("li a");
 const currentURL = window.location.href;
 navLinks.forEach((link) => {
 if (link.href === currentURL) {
